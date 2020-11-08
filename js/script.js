9eadf8f636d07ba8d1b65a14cf293e1d357294ab
Medine <you@example.com>
2020-11-08 17:42:13 +0100

    changed background for some elements

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ imgButton.setAttribute('class','btn-class');
 //changed the style for the created button.
 imgButton.style.backgroundColor = 'blue';
 imgButton.style.padding = '.5rem 1.2rem';
-imgButton.style.marginTop = '1rem';
+imgButton.style.marginTop = '0.8rem';
 imgButton.style. width = '10rem'; 
 imgButton.innerText = 'change picture';
 art2Element.insertAdjacentElement('beforeend',imgButton);
@@ -29,4 +29,33 @@ imgButton.addEventListener('click', function(event){
     img1Element.src= 'img/hoodie-ocean.png';
     img2Element.src= 'img/hoodie-ash.png';
     img3Element.src= 'img/hoodie-fire.png'; 
+});
+
+
+
+//Ändra bakgrundsfärg på minst ett element
+
+
+//selected the parent element
+let navElement = document.querySelector('#header-navigation');
+//created a new button.
+let headerButton = document.createElement('button');
+//changed the style for the created button.
+headerButton.style.backgroundColor = 'red';
+headerButton.style.padding = '.3rem .5rem';
+headerButton.style. width = '18rem'; 
+headerButton.innerText = 'change background for some elements';
+navElement.insertAdjacentElement('afterBegin',headerButton);
+//when you click on the red created button ===> change background for all a -element, all p -elements and first button (blue), witch I creaced
+headerButton.addEventListener('click', function(event){
+    let aElement = document.querySelectorAll('a');
+    for (let i = 0; i<aElement.length; i++ ){
+        aElement[i].style.backgroundColor = 'red';
+    }
+    let pElement = document.querySelectorAll('p');
+    for (let i = 0; i<pElement.length; i++ ){
+        pElement[i].style.backgroundColor = 'blue';
+    }
+    imgButton.style.backgroundColor = 'green';
+    
 });
